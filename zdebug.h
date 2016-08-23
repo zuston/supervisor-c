78f9d0bc1d7d1783fd9edc4667fdac01a9a9352a
zuston.shacha <zuston.shacha@gmail.com>
2016-08-23 23:14:06 +0800

    modify tip

--- zdebug.h
+++ zdebug.h
@@ -5,8 +5,8 @@
 #include <stdlib.h>
 
 
-#define SUCCESS fprintf(stderr,"[SUCCESS] %s:%d ",__FILE__,__LINE__)
-#define ERROR fprintf(stderr,"[ERROR] %s:%d ",__FILE__,__LINE__)
+#define SUCCESS fprintf(stderr,"[SUCCESS] %s:%d \n",__FILE__,__LINE__)
+#define ERROR fprintf(stderr,"[ERROR] %s:%d \n",__FILE__,__LINE__)
 
 
 #endif
